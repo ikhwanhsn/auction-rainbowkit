921b5a6b03ea4efa3130d56985e40b4bd1d24c1a
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-02-04 15:08:09 +0700

    chore: add detail auction call function

--- src/app/(pages)/auction/[contract]/page.tsx
+++ src/app/(pages)/auction/[contract]/page.tsx
@@ -1,10 +1,68 @@
 "use client";
 
+import { contractABISimpleAuction } from "@/services/abi";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import { useReadContracts } from "wagmi";
 
 const AuctionPage = () => {
-  const { contract } = useParams();
+  const params = useParams();
+  const contract = Array.isArray(params.contract)
+    ? params.contract[0]
+    : params.contract;
+  if (!contract || !contract.startsWith("0x")) {
+    return <p>Invalid contract address.</p>;
+  }
+  const contractAddress = contract as `0x${string}` | undefined;
+  const {
+    data,
+    isPending,
+    refetch,
+  }: { data: any; isPending: boolean; refetch: () => void } = useReadContracts({
+    contracts: [
+      {
+        address: contractAddress,
+        abi: contractABISimpleAuction,
+        functionName: "highestBid",
+      },
+      {
+        address: contractAddress,
+        abi: contractABISimpleAuction,
+        functionName: "highestBidder",
+      },
+      {
+        address: contractAddress,
+        abi: contractABISimpleAuction,
+        functionName: "pendingReturn",
+      },
+      {
+        address: contractAddress,
+        abi: contractABISimpleAuction,
+        functionName: "beneficiary",
+      },
+      {
+        address: contractAddress,
+        abi: contractABISimpleAuction,
+        functionName: "auctionEndTime",
+      },
+    ],
+  });
+
+  const [
+    highestBid,
+    highestBidder,
+    pendingReturn,
+    beneficiary,
+    auctionEndTime,
+  ] = data || [];
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
   return (
     <main className="min-h-screen p-5">
       <Link href="/" className="btn btn-primary text-white btn-sm">
@@ -12,6 +70,31 @@ const AuctionPage = () => {
       </Link>
       <h1 className="text-3xl mt-3">Auction</h1>
       <p>{contract}</p>
+      <p>
+        Autcion end time:{" "}
+        {auctionEndTime && !isPending && auctionEndTime.result}
+      </p>
+      <p>Beneficiary: {beneficiary && !isPending && beneficiary.result}</p>
+      <p>Highest bid: {highestBid && !isPending && highestBid.result} ETH</p>
+      <p>
+        Highest bidder:{" "}
+        {highestBidder &&
+        !isPending &&
+        highestBidder.result === "0x0000000000000000000000000000000000000000"
+          ? "-"
+          : highestBidder.result}
+      </p>
+      <p>
+        Pending return:{" "}
+        {(pendingReturn && !isPending && pendingReturn.result) || "0"} ETH
+      </p>
+      <input
+        type="number"
+        placeholder="10 ETH"
+        className="input input-primary bg-white"
+      />
+      <button className="btn btn-primary text-white">Bid</button>
+      <button className="btn btn-primary text-white">Withdraw</button>
     </main>
   );
 };
--- src/services/contractAddress.ts
+++ src/services/contractAddress.ts
@@ -1,5 +1,6 @@
 const contractAddressAuctionFactory =
   "0xAe76f80f8342F1145e0287CbD2b910bc278cBba0";
-const contractAddressSimpleAuction = "";
+const contractAddressSimpleAuction =
+  "0x25B3b9534Ae6A47710C80bFB08cC59E35Bd492C6";
 
 export { contractAddressAuctionFactory, contractAddressSimpleAuction };

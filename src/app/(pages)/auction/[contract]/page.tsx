9d064776ae7ef03831f43e8867e051bc9e7a6f02
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-02-06 11:33:03 +0700

    chore: update UI and clipboard func

--- src/app/(pages)/auction/[contract]/page.tsx
+++ src/app/(pages)/auction/[contract]/page.tsx
@@ -14,6 +14,7 @@ import {
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
 import { ToastContainer, toast } from "react-toastify";
+import { LuCopy, LuCopyCheck } from "react-icons/lu";
 
 dayjs.extend(duration);
 
@@ -28,6 +29,10 @@ const AuctionPage = () => {
   const [auctionEnd, setAuctionEnd] = useState<dayjs.Dayjs | null>(null);
   const [remainingTime, setRemainingTime] = useState<string>("");
   const [timeCreationContract, setTimeCreationContract] = useState<string>("");
+  const [isCopied1, setIsCopied1] = useState<boolean>(false);
+  const [isCopied2, setIsCopied2] = useState<boolean>(false);
+  const [isCopied3, setIsCopied3] = useState<boolean>(false);
+  const [idAuction, setIdAuction] = useState<string>("");
 
   // Read data from smart contract
   const {
@@ -94,6 +99,27 @@ const AuctionPage = () => {
       hash,
     });
 
+  // Copy to clipboard
+  const copyToClipboard = (text: string, no: number) => {
+    navigator.clipboard.writeText(text);
+    if (no === 1) {
+      setIsCopied1(true);
+      setTimeout(() => {
+        setIsCopied1(false);
+      }, 2000);
+    } else if (no === 2) {
+      setIsCopied2(true);
+      setTimeout(() => {
+        setIsCopied2(false);
+      }, 2000);
+    } else {
+      setIsCopied3(true);
+      setTimeout(() => {
+        setIsCopied3(false);
+      }, 2000);
+    }
+  };
+
   // Set auction end time
   useEffect(() => {
     if (data && auctionEndTime?.result) {
@@ -151,54 +177,148 @@ const AuctionPage = () => {
     }
   }, [isConfirmed]);
 
+  useEffect(() => {
+    setIdAuction(String(localStorage.getItem("idAuction")));
+  }, []);
+
   return (
     <>
-      {isConnected && (
-        <main className="min-h-screen p-5">
+      {isConnected ? (
+        <main className="min-h-screen p-6 bg-gray-100 flex flex-col items-center">
           <ToastContainer />
-          <Link href="/" className="btn btn-primary text-white btn-sm">
-            Back
-          </Link>
-          <h1 className="text-3xl mt-3">Auction</h1>
-          <p>{contract}</p>
-          <p>
-            Auction End Time:{" "}
-            {auctionEnd
-              ? auctionEnd.format("DD/MM/YYYY HH:mm:ss")
-              : "Not Set or Invalid"}
-          </p>
-          <p>
-            Remaining Time:{" "}
-            {remainingTime || "0 days 0 hours 0 minutes 0 seconds"}
-          </p>
-          <p>Beneficiary: {beneficiary?.result}</p>
-          <p>Highest bid: {highestBid?.result} ETH</p>
-          <p>
-            Highest bidder:{" "}
-            {highestBidder?.result ===
-            "0x0000000000000000000000000000000000000000"
-              ? "-"
-              : highestBidder?.result}
-          </p>
-          <p>Pending return: {pendingReturn?.result || "0"} ETH</p>
-          <form onSubmit={submit}>
-            <input
-              type="number"
-              name="bid"
-              placeholder="10 ETH"
-              value={timeCreationContract}
-              onChange={(e) => setTimeCreationContract(e.target.value)}
-              required
-              className="input input-primary bg-white"
-            />
-            <button className="btn btn-primary text-white" type="submit">
-              Bid
+          <div className="w-full max-w-2xl bg-white p-6 rounded-lg shadow-lg">
+            <div className="flex justify-between items-center mb-4">
+              <Link
+                href="/"
+                className="px-4 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700"
+              >
+                Back
+              </Link>
+              <h1 className="text-3xl font-bold text-gray-800">
+                Auction #{idAuction}
+              </h1>
+            </div>
+            <div className="space-y-2 text-gray-700">
+              <p className="text-lg font-semibold flex items-center gap-2">
+                Contract:{" "}
+                <Link
+                  href={`https://sepolia.etherscan.io/address/${contract}`}
+                  target="_blank"
+                  className="text-gray-900 font-normal hover:text-blue-500 hover:underline"
+                >
+                  {contract}
+                </Link>{" "}
+                {isCopied1 ? (
+                  <LuCopyCheck className="text-green-500" />
+                ) : (
+                  <LuCopy
+                    className="hover:text-blue-500 cursor-pointer"
+                    onClick={() => copyToClipboard(contract as string, 1)}
+                  />
+                )}
+              </p>
+              <p className="text-lg font-semibold">
+                Auction End Time:{" "}
+                <span className="font-normal">
+                  {auctionEnd
+                    ? auctionEnd.format("DD/MM/YYYY HH:mm:ss")
+                    : "Not Set or Invalid"}
+                </span>
+              </p>
+              <p className="text-lg font-semibold">
+                Remaining Time:{" "}
+                <span className="font-normal">
+                  {remainingTime || "0 days 0 hours 0 minutes 0 seconds"}
+                </span>
+              </p>
+              <p className="text-lg flex items-center gap-2 font-semibold">
+                Beneficiary:{" "}
+                <span className="font-normal flex items-center gap-2">
+                  <Link
+                    href={`https://sepolia.etherscan.io/address/${beneficiary?.result}`}
+                    target="_blank"
+                    className="text-gray-900 font-normal hover:text-blue-500 hover:underline"
+                  >
+                    {beneficiary?.result}
+                  </Link>{" "}
+                  {isCopied2 ? (
+                    <LuCopyCheck className="text-green-500" />
+                  ) : (
+                    <LuCopy
+                      className="hover:text-blue-500 cursor-pointer"
+                      onClick={() => copyToClipboard(beneficiary?.result, 2)}
+                    />
+                  )}
+                </span>
+              </p>
+              <p className="text-lg font-semibold">
+                Highest Bid:{" "}
+                <span className="font-normal">{highestBid?.result} ETH</span>
+              </p>
+              <p className="text-lg font-semibold flex items-center gap-2">
+                Highest Bidder:{" "}
+                <span className="font-normal flex items-center gap-2">
+                  {highestBidder?.result ===
+                  "0x0000000000000000000000000000000000000000" ? (
+                    "-"
+                  ) : (
+                    <Link
+                      href={`https://sepolia.etherscan.io/address/${highestBidder?.result}`}
+                      target="_blank"
+                      className="text-gray-900 font-normal hover:text-blue-500 hover:underline"
+                    >
+                      {highestBidder?.result}
+                    </Link>
+                  )}
+                  {isCopied3 ? (
+                    <LuCopyCheck className="text-green-500" />
+                  ) : (
+                    highestBidder?.result !==
+                      "0x0000000000000000000000000000000000000000" && (
+                      <LuCopy
+                        className="hover:text-blue-500 cursor-pointer"
+                        onClick={() =>
+                          copyToClipboard(highestBidder?.result, 3)
+                        }
+                      />
+                    )
+                  )}
+                </span>
+              </p>
+              <p className="text-lg font-semibold">
+                Pending Return:{" "}
+                <span className="font-normal">
+                  {pendingReturn?.result || "0"} ETH
+                </span>
+              </p>
+            </div>
+            <form onSubmit={submit} className="mt-4 space-y-3">
+              <input
+                type="number"
+                name="bid"
+                placeholder="10 ETH"
+                value={timeCreationContract}
+                onChange={(e) => setTimeCreationContract(e.target.value)}
+                required
+                className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-white"
+              />
+              <button
+                className="w-full px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all"
+                type="submit"
+              >
+                Place Bid
+              </button>
+            </form>
+            <button className="w-full mt-3 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition-all">
+              Withdraw
             </button>
-          </form>
-          <button className="btn btn-primary text-white">Withdraw</button>
+          </div>
         </main>
+      ) : (
+        <p className="text-center text-lg font-semibold text-red-600 mt-10">
+          Please connect your wallet to participate in the auction.
+        </p>
       )}
-      {!isConnected && <p>Please connect wallet!</p>}
     </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,16 +84,104 @@ const Home = () => {
   }, [isConfirmed]);
 
   return (
+    // <>
+    //   {isConnected && (
+    //     <main className="px-5 py-7">
+    //       <ToastContainer />
+    //       <section className="flex justify-between items-center">
+    //         <h1 className="text-2xl font-semibold">List of Auctions</h1>
+    //         <aside className="space-x-2">
+    //           {/* Button Create Auction */}
+    //           <button
+    //             className="btn btn-sm btn-primary text-white"
+    //             onClick={() => {
+    //               const modal = document.getElementById(
+    //                 "my_modal_2"
+    //               ) as HTMLDialogElement;
+    //               modal?.showModal();
+    //             }}
+    //           >
+    //             Create Auction
+    //           </button>
+    //           {/* Open modal button create auction */}
+    //           <dialog id="my_modal_2" className="modal ">
+    //             <div className="modal-box bg-white text-black">
+    //               <h3 className="font-bold text-lg">Hello!</h3>
+
+    //               <p className="py-4">
+    //                 Press ESC key or click outside to close
+    //               </p>
+    //               <form onSubmit={submit}>
+    //                 <input
+    //                   type="number"
+    //                   name="tokenId"
+    //                   className="input input-bordered w-full bg-white input-primary"
+    //                   placeholder="Time in seconds"
+    //                   value={timeCreationContract}
+    //                   onChange={(e) => setTimeCreationContract(e.target.value)}
+    //                   required
+    //                 />
+    //                 <section className="space-x-1 mt-2">
+    //                   <button
+    //                     className="btn btn-primary text-white"
+    //                     type="submit"
+    //                     disabled={isPending}
+    //                   >
+    //                     {isPending ? "Creating..." : "Create"}
+    //                   </button>
+    //                   <button
+    //                     className="btn btn-error text-white"
+    //                     type="button"
+    //                     onClick={() => {
+    //                       const modal = document.getElementById(
+    //                         "my_modal_2"
+    //                       ) as HTMLDialogElement;
+    //                       modal?.close();
+    //                     }}
+    //                   >
+    //                     Cancel
+    //                   </button>
+    //                 </section>
+    //               </form>
+    //             </div>
+    //             <form method="dialog" className="modal-backdrop">
+    //               <button>close</button>
+    //             </form>
+    //           </dialog>
+    //           <button className="btn btn-sm btn-success text-white">
+    //             My Auction
+    //           </button>
+    //         </aside>
+    //       </section>
+    //       <p>{isFetching && "Loading..."}</p>
+    //       {/* List of auctions */}
+    //       <section className="mt-3 gap-1 grid grid-cols-3">
+    //         {dataAuction?.map((auction, index) => (
+    //           <Link
+    //             href={`/auction/${auction}`}
+    //             key={index}
+    //             className="card w-full border px-5 py-3 shadow-sm hover:bg-gray-50 hover:shadow-md"
+    //           >
+    //             {auction}
+    //           </Link>
+    //         ))}
+    //       </section>
+    //     </main>
+    //   )}
+    //   {!isConnected && <p>Please connect wallet!</p>}
+    // </>
     <>
-      {isConnected && (
-        <main className="px-5 py-7">
+      {isConnected ? (
+        <main className="px-6 py-8 bg-gray-100 min-h-screen">
           <ToastContainer />
-          <section className="flex justify-between items-center">
-            <h1 className="text-2xl font-semibold">List of Auctions</h1>
-            <aside className="space-x-2">
+          <section className="flex justify-between items-center bg-white p-5 rounded-lg shadow-md">
+            <h1 className="text-3xl font-bold text-gray-800">
+              List of Auctions
+            </h1>
+            <aside className="flex space-x-3">
               {/* Button Create Auction */}
               <button
-                className="btn btn-sm btn-primary text-white"
+                className="px-4 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700"
                 onClick={() => {
                   const modal = document.getElementById(
                     "my_modal_2"
@@ -101,74 +189,90 @@ const Home = () => {
                   modal?.showModal();
                 }}
               >
-                Create Auction
+                + Create Auction
               </button>
-              {/* Open modal button create auction */}
-              <dialog id="my_modal_2" className="modal ">
-                <div className="modal-box bg-white text-black">
-                  <h3 className="font-bold text-lg">Hello!</h3>
-
-                  <p className="py-4">
-                    Press ESC key or click outside to close
-                  </p>
-                  <form onSubmit={submit}>
-                    <input
-                      type="number"
-                      name="tokenId"
-                      className="input input-bordered w-full bg-white input-primary"
-                      placeholder="Time in seconds"
-                      value={timeCreationContract}
-                      onChange={(e) => setTimeCreationContract(e.target.value)}
-                      required
-                    />
-                    <section className="space-x-1 mt-2">
-                      <button
-                        className="btn btn-primary text-white"
-                        type="submit"
-                        disabled={isPending}
-                      >
-                        {isPending ? "Creating..." : "Create"}
-                      </button>
-                      <button
-                        className="btn btn-error text-white"
-                        type="button"
-                        onClick={() => {
-                          const modal = document.getElementById(
-                            "my_modal_2"
-                          ) as HTMLDialogElement;
-                          modal?.close();
-                        }}
-                      >
-                        Cancel
-                      </button>
-                    </section>
-                  </form>
-                </div>
-                <form method="dialog" className="modal-backdrop">
-                  <button>close</button>
-                </form>
-              </dialog>
-              <button className="btn btn-sm btn-success text-white">
-                My Auction
+              <button className="px-4 py-2 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700">
+                My Auctions
               </button>
             </aside>
           </section>
-          <p>{isFetching && "Loading..."}</p>
+
+          {/* Modal */}
+          <dialog id="my_modal_2" className="modal">
+            <div className="modal-box bg-white p-6 rounded-lg shadow-lg text-gray-800">
+              <h3 className="text-xl font-bold">Create New Auction</h3>
+              <p className="text-sm text-gray-500 mt-1">
+                Set the duration for the auction in seconds.
+              </p>
+              <form onSubmit={submit} className="mt-4 space-y-3">
+                <input
+                  type="number"
+                  name="tokenId"
+                  className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 bg-white"
+                  placeholder="Enter time in seconds"
+                  value={timeCreationContract}
+                  onChange={(e) => setTimeCreationContract(e.target.value)}
+                  required
+                />
+                <div className="flex justify-end space-x-2">
+                  <button
+                    className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+                    type="submit"
+                    disabled={isPending}
+                  >
+                    {isPending ? "Creating..." : "Create"}
+                  </button>
+                  <button
+                    className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
+                    type="button"
+                    onClick={() => {
+                      const modal = document.getElementById(
+                        "my_modal_2"
+                      ) as HTMLDialogElement;
+                      modal?.close();
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            </div>
+          </dialog>
+
+          <p className="text-center text-gray-600 mt-4">
+            {isFetching && "Loading..."}
+          </p>
+
           {/* List of auctions */}
-          <section className="mt-3 gap-1 grid grid-cols-3">
-            {dataAuction?.map((auction, index) => (
-              <Link
-                href={`/auction/${auction}`}
-                key={index}
-                className="card w-full border px-5 py-3 shadow-sm hover:bg-gray-50 hover:shadow-md"
-              >
-                {auction}
-              </Link>
-            ))}
+          <section className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {[...(dataAuction || [])].reverse().map((auction, index, arr) => {
+              const auctionNumber = (arr.length - index)
+                .toString()
+                .padStart(5, "0");
+
+              return (
+                <Link
+                  href={`/auction/${auction}`}
+                  key={index}
+                  className="p-5 bg-white rounded-lg shadow-md border hover:shadow-lg transition-all"
+                  onClick={() => {
+                    localStorage.setItem("idAuction", auctionNumber);
+                  }}
+                >
+                  <section className="text-lg font-semibold text-gray-800 tracking-tight">
+                    <p>Auction #{auctionNumber}</p>
+                    <span className="text-base font-normal">{auction}</span>
+                  </section>
+                </Link>
+              );
+            })}
           </section>
         </main>
+      ) : (
+        <p className="text-center text-lg font-semibold text-red-600 mt-10">
+          Please connect your wallet to view auctions!
+        </p>
       )}
-      {!isConnected && <p>Please connect wallet!</p>}
     </>
   );
 };

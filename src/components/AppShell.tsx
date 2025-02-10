c528477cc6809c52c76e6e2c74558ea66b5af059
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-02-10 11:05:51 +0700

    fix: footer

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,12 +1,22 @@
+"use client";
+
+import { useAccount } from "wagmi";
 import Navbar from "./Navbar";
+import { usePathname } from "next/navigation";
 
 const AppShell = ({ children }: { children: React.ReactNode }) => {
+  const { isConnected } = useAccount();
+  const pathname = usePathname();
   return (
     <main className="min-h-screen relative">
       <Navbar />
       <section className="h-16 w-full"></section>
       {children}
-      <footer className="bg-white text-black text-center p-4 text-sm fixed bottom-0 w-full">
+      <footer
+        className={`bg-white text-black text-center p-4 text-sm ${
+          isConnected && pathname === "/" ? "" : "fixed"
+        } bottom-0 w-full`}
+      >
         Copyright &copy; 2025 Solidity Auction Factory by SmartWeb3 ID
       </footer>
     </main>
